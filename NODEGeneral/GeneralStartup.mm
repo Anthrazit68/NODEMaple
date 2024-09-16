74a545d6ef64c06aec59cb15b831bf98677e7b11
Andreas Zieritz <andreas.zieritz@node.no>
2024-09-16 20:44:33 +0200

    Moved code for NODETimber back to Maple workbook

--- NODEGeneral/GeneralStartup.mm
+++
@@ -1,9 +0,0 @@
-# se https://www.mapleprimes.com/questions/228884-Getting-List-Of-Properties-And-Items-Of-A-Library
-kernelopts(opaquemodules = false);
-
-# loading necessary libraries
-with(NODEFunctions);
-with(NODETimberEN1995);
-with(DocumentTools);
-with(StringTools);
-with(Units[Simple]);
