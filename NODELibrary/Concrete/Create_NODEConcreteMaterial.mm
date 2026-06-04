20a07825d5c64f807b862276cca5f22f7ed4a46e
Anthrazit68 <andreas@zieritz.no>
2026-06-04 22:59:30 +0200

    Concrete material

--- NODELibrary/Concrete/Create_NODEConcreteMaterial.mm
+++ NODELibrary/Concrete/Create_NODEConcreteMaterial.mm
@@ -1,49 +1,65 @@
-# Create_NODEConcreteMaterial
-# Based on Creating the AISC Shapes Database Package, Samir Khan (skhan@maplesoft.com) , September 2019. 
-# v 0.2
-# Andreas Zieritz
+# Create_NODEConcreteMaterial.mm
+# Copyright (C) 2026  Andreas Zieritz
 
-# Program reads materialdatabase and writes to Maple library
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
 
-# Importing and Parsing Data
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-data:=convert(ExcelTools:-Import("Data/Materialdata.xlsx","concrete","A1:D21"), Matrix):
-data:=subs("&ndash;" = NULL,data):
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# This is the metadata from the spreadsheet
-# - ingen dots in variablename!
+# Create_NODEConcreteMaterial.mm
 
-# reading f_ck and f_ck,cube - all other values are calculated
-metadata:=[[A, "strengthclass_NS", 1, "Strength class NS, e.g. B35, LB30"]
-		,[B, "strengthclass_CEN", 1, "Strength class CEN, e.g. C20/25, LC25/28"]
-		,[C, "f_ck", (MPa), "betongens karakteristiske sylindertrykkfasthet etter 28 døgn"]
-		,[D, "f_ck,cube", (MPa), "betongens karakteristiske terningtrykkfasthet etter 28 døgn"]]:
+proc()
+    local rawData, metadata, dataTable, i, j, outputFile, outputFilename, currentClass, concreteProperties;
+    uses ExcelTools;
 
-# ,[E, "f_cm", (MPa), "middelverdi av betongens sylindertrykkfasthet"]
-# ,[F, "f_ctm", (MPa), "middelverdi av betongens aksialstrekkfasthet"]
-# ,[G, "f_ctk,0,05", (MPa), "betongens karakteristiske aksialstrekkfasthet"]
-# ,[H, "f_ctk,0,95", (MPa), "betongens karakteristiske aksialstrekkfasthet"]
-# ,[I, "E_cm", (GPa), "sekantmodul, elastisitetsmodul for betong"]
-# ,[J, "epsilon_c1", 1, "trykktøyning i betongen ved største spenning fc"]
-# ,[K, "epsilon_cu1", 1, "tøyningsgrense for trykk i betongen"]
-# ,[L, "epsilon_c2", 1, "trykktøyning i betongen"]
-# ,[M, "epsilon_cu2", 1, "tøyningsgrense for trykk i betongen"]
-# ,[N, "n", 1, " "]
-# ,[O, "epsilon_c3", 1, "trykktøyning i betongen"]
-# ,[P, "epsilon_cu3", 1, "tøyningsgrense for trykk i betongen"]]:
+    # Dynamic import: Only reads columns A to D (the first 4 columns)
+    rawData := convert(ExcelTools:-Import("Data/Materialdata.xlsx", "concrete", "A1:D"), Matrix):
+    rawData := subs("&ndash;" = NULL, rawData):
 
+    # Metadata describing columns, units, and descriptions for the first 4 columns
+    metadata := [
+         [A, "strengthclass_NS", 1, "Strength class NS, e.g. B35, LB30"]
+        ,[B, "strengthclass_CEN", 1, "Strength class CEN, e.g. C20/25, LC25/28"]
+        ,[C, "f_ck", (MPa), "characteristic cylinder compressive strength after 28 days"]
+        ,[D, "f_ck,cube", (MPa), "characteristic cube compressive strength after 28 days"]
+    ]:
 
-# Create a table data structure and read the material data into it
-# i...number of rows with values
-# j...number of columns with values
+    dataTable := table():
 
-# if all values required, change j = 1..4 to j = 1..16
-dataTable:=table():
-temp:=seq(
-	dataTable[data[i,1]] = 
-	table([ 
-		seq(metadata[j,2] = 
-			`if`(data[i,j]<>NULL, data[i,j]*Unit(metadata[j,3]), NULL) 
-		,j = 1..4)
-		 ])
-	,i=2..21):
+    # Populate the table starting from row 2
+    for i from 2 to numelems(rawData[.., 1]) do
+        currentClass := rawData[i, 1];
+
+        # Strict type-check: ensures we skip ghost rows, NULL values, or numeric zeros (like 0.)
+        if currentClass <> NULL and currentClass <> "" and type(currentClass, string) then
+            
+            # Map parameters. We use j = 3..4 to apply units to f_ck and f_ck,cube
+            concreteProperties := table([
+                "strengthclass_CEN" = rawData[i, 2],
+                seq(metadata[j, 2] = `if`(rawData[i, j]<>NULL and rawData[i, j]<>-"" and rawData[i, j]<>" -", 
+                                         rawData[i, j]*Unit(metadata[j, 3]), NULL), j = 3..4)
+            ]);
+
+            # Store inside the datatable with proper evaluation
+            dataTable[currentClass] := eval(concreteProperties);
+        end if;
+    end do:
+
+    # Export everything safely to Data_ConcreteMaterial.mm
+    outputFilename := "Concrete/Data_ConcreteMaterial.mm";
+    outputFile := FileTools[Text][Open](outputFilename, create=true, overwrite=true);
+
+    FileTools[Text][WriteString](outputFile, sprintf("metadata := %a:\n", eval(metadata)));
+    FileTools[Text][WriteString](outputFile, sprintf("dataTable := %a:\n", eval(dataTable)));
+
+    FileTools[Text][Close](outputFile);
+
+end proc(): # Executed immediately upon building
--- NODELibrary/Concrete/NODEConcreteMaterial.mm
+++ NODELibrary/Concrete/NODEConcreteMaterial.mm
@@ -1,5 +1,5 @@
-# NODEConcreteMaterial : definition of concrete material properties
-# Copyright (C) 2024  Andreas Zieritz
+# NODEConcreteMaterial.mm
+# Copyright (C) 2026  Andreas Zieritz
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,58 +14,58 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-NODEConcreteMaterial:= module ()
-   export Property, Exposureclasses, Durabilityclasses: 
-   option package;  
+NODEConcreteMaterial := module()
+    description "Material properties for concrete according to Eurocode 2";
+    option package;  
+    export Property, Exposureclasses, Durabilityclasses; 
 
-   local parNames, memberNames, NODEMetadata, NODEData, NODETable:
+    # Fully encapsulated on module level - zero global variables!
+    local metadata, dataTable, parNames, memberNames;
 
-   NODEMetadata := metadata:
-   NODEData := data:
-   NODETable := eval(dataTable,1):
+# $include MUST be at the absolute start of the line (column 1) to build properly
+$include "Concrete/Data_ConcreteMaterial.mm"
 
-   parNames:=convert(metadata[2..,2],list):
-   memberNames:=convert(NODEData[2..,1],list):
+    parNames := convert(metadata[2.., 2], list):
+    
+    # Extracts all concrete grades and sorts them perfectly (e.g., B20, B30, B35...)
+    memberNames := sort([indices(dataTable, 'nolist')], NODEFunctions:-SortStructuralnames);
 
-   Property:=proc(requiredMember::string,requiredPar::string)
+    Property := proc(requiredMember::string, requiredPar::string)
+        uses ListTools;
 
-      local parPos, memberPos:
+        if _npassed = 2 then
+            if assigned(dataTable[requiredMember]) then
+                if assigned(dataTable[requiredMember][requiredPar]) then
+                    return dataTable[requiredMember][requiredPar];
+                else
+                    error("Parameter not found: %1", requiredPar);
+                end if;
+            else
+                error("Concrete strength class not found: %1", requiredMember);
+            end if;
 
-      if _npassed = 2 then
-         if requiredMember in memberNames and requiredPar in parNames then
-           NODETable[requiredMember][requiredPar]:
-         else
-            if not requiredMember in memberNames and not requiredPar in parNames then
-               error("Member and parameter not found"):
-            elif not requiredMember in memberNames then
-               error("Member not found"):
-            elif not requiredPar in parNames then
-               error("Parameter not found"):
-            end if:
-         end if:
+        elif _npassed = 3 and _passed[3] = "metadata" then
+            local parPos := ListTools:-Search(requiredPar, parNames);
+            if parPos > 0 then
+                return metadata[parPos, 4]; # Column 4 contains description string
+            else
+                error("Parameter not found in metadata");
+            end if;
 
-      elif _npassed = 3 and _passed[3] = "metadata" then
-         parPos:=ListTools:-Search(requiredPar, parNames):
-         return NODEMetadata[parPos+1,5]:
+        elif _npassed = 1 and _passed[1] = "allmembers" then
+            return memberNames;
 
-      elif _npassed = 1 and _passed[1] = "allmembers" then
-         return memberNames:
+        elif _npassed = 1 and _passed[1] = "metadata" then
+            return parNames;       
+        end if;
+    end proc:
 
-	elif _npassed = 1 and _passed[1] = "metadata" then
-         return parNames:       
+    Exposureclasses := proc()::list;
+        return ["X0", "XC1", "XC2", "XC3", "XC4", "XD1", "XD2", "XD3", "XS1", "XS2", "XS3", "XF1", "XF2", "XF3", "XF4", "XA1", "XA2", "XA3"];
+    end proc:
 
-      end if:
-
-	end proc:
-
-
-	Exposureclasses := proc()::list;
-		return ["X0", "XC1", "XC2", "XC3", "XC4", "XD1", "XD2", "XD3", "XS1", "XS2", "XS3", "XF1", "XF2", "XF3", "XF4", "XA1", "XA2", "XA3"]
-	end proc:
-
-
-	Durabilityclasses := proc()::list;
-		return ["M90", "M60", "M45", "MF45", "M40", "MF40"]
-	end proc:
-	
+    Durabilityclasses := proc()::list;
+        return ["M90", "M60", "M45", "MF45", "M40", "MF40"];
+    end proc:
+    
 end module:
